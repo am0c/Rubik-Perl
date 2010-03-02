cb3503e8d0095fa785bd4c3970cda908511659b0
Petrea Stefan Corneliu <randomcoder1@gmail.com>
2010-03-02 05:13:50 -0500

    added 2 more permutation generators

--- SJT_xs/benchmark.pl
+++ SJT_xs/benchmark.pl
@@ -1,15 +1,61 @@
 use strict;
 use warnings;
-use Benchmark qw/timethese/;
+use Benchmark qw/cmpthese timethese/;
 use Algorithm::Permute;
 use SJT;
 
-
-
 my $n = 8; # objects to permute
+my $iter=4;
 
-timethese(
-	20,
+sub nextPermute(\@)
+{
+	my( $vals )= @_;
+	my $last= $#{$vals};
+	return ""   if  $last < 1;
+	# Find last item not in reverse-sorted order:
+	my $i= $last-1;
+	$i--   until  $i < 0  ||  $vals->[$i] lt $vals->[$i+1];
+	# If complete reverse sort, we are done!
+	return ""   if  -1 == $i;
+	# Re-sort the reversely-sorted tail of the list:
+	@{$vals}[$i+1..$last]= reverse @{$vals}[$i+1..$last]
+	if  $vals->[$i+1] gt $vals->[$last];
+	# Find next item that will make us "greater":
+	my $j= $i+1;
+	$j++  until  $vals->[$i] lt $vals->[$j];
+	# Swap:
+	@{$vals}[$i,$j]= @{$vals}[$j,$i];
+	return 1;
+}
+
+sub make_orderings
+{
+	my $num = shift;
+
+	my @arr = (1 .. $num);
+
+	return sub {
+		my $last = $#arr;
+
+		my $i = $last - 1;
+		$i-- while 0 <= $i && $arr[$i] >= $arr[$i+1];
+		return if $i == -1;
+
+		@arr[$i+1..$last] = reverse @arr[$i+1..$last]
+		if $arr[$i+1] > $arr[$last];
+
+		my $j=$i+1;
+		$j++ while $arr[$i] >= $arr[$j];
+
+		@arr[$i,$j] = @arr[$j,$i];
+
+		return @arr;
+	}
+}
+
+
+cmpthese(
+	$iter,
 	{
 		'Algorithm::Permute' => sub {
 			use Algorithm::Permute;
@@ -23,6 +69,18 @@ timethese(
 			while($s->next_perm()){
 				my @p = @{$s->{permutation}};
 				#$s->print_perm;
+			};
+		},
+		'tye\'s permutation generator' => sub {
+
+
+			my @w= (1..$n);
+			do {
+			} while( nextPermute(@w) );
+		},
+		'drangonchild\'s perm generator' => sub {
+			my $i = make_orderings($n);
+			while(my @a = $i->()){
 			};
 		},
 	}
