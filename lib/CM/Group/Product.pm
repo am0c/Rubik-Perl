7d1eb5b6d4f2834648d1ad025d6d27f04d2b03d5
Stefan Petrea <stefan.petrea@gmail.com>
2010-03-10 12:49:04 +0000

    added product group

---
+++ lib/CM/Group/Product.pm
@@ -0,0 +1,61 @@
+package CM::Group::Product;
+use Moose;
+use CM::Tuple;
+use strict;
+use warnings;
+with 'CM::Group' => { element_type => 'CM::Tuple'  };
+
+
+has groupG => (
+	isa => 'CM::Group',
+	is => 'rw',
+	required => 1,
+);
+
+has groupH => (
+	isa => 'CM::Group',
+	is => 'rw',
+	required => 1,
+);
+
+
+sub operation {
+    my ($self,$a,$b) = @_;
+    my $first  = $self->groupG->operation($a->first ,$b->first);
+    my $second = $self->groupH->operation($a->second,$b->second);
+    return CM::Tuple->new({
+		    first => $first,
+		    second=> $second,
+    });
+}
+
+
+# because the direct product of groups has as many elements as the product of the groups
+sub _builder_order {
+    my ($self) = @_;
+    return $self->groupG->_builder_order * $self->groupH->_builder_order;
+}
+
+
+sub compute_elements {
+	my ($self) = @_;
+	my @elements;
+
+	$self->groupG->compute_elements unless @{$self->groupG->elements};
+	$self->groupH->compute_elements unless @{$self->groupH->elements};
+
+	for my $g (@{$self->groupG->elements}) {
+		for my $h (@{$self->groupH->elements}) {
+			$self->add_to_elements(
+				CM::Tuple->new({
+						first =>$g,
+						second=>$h,
+				})
+			);
+		};
+	};
+};
+
+
+
+1;
