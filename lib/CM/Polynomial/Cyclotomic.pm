c9c60ac4013a52b6e7add1651c8b138bbb3c5e1c
Stefan Petrea <stefan.petrea@gmail.com>
2010-03-31 06:17:07 +0300

    changed method for case III for cyclotomic polynomials

--- lib/CM/Polynomial/Cyclotomic.pm
+++ lib/CM/Polynomial/Cyclotomic.pm
@@ -6,38 +6,63 @@ use Math::Polynomial;
 use Math::Factor::XS qw( factors);
 use Math::Prime::XS  qw( is_prime);
 use Math::BigInt;
-use List::AllUtils qw/reduce/;
+use List::AllUtils qw/reduce first/;
 use File::Slurp qw/slurp/;
+use Cwd;
+use Path::Class qw/file/;
+extends 'Math::Polynomial';
 
-BEGIN {
-	my @mu;
 
-	# the file contains the precomputed Möbius function mu(x) up to 10^5 we won't need more
-	# table located right here ---> http://www.research.att.com/njas/sequences/A008683
-	@mu = split(/,/,slurp('Möbius.data'));
 
-	@CM::Polynomial::Cyclotomic::mu = @mu;
-};
 
+
+my @mu;
+
+# the file contains the precomputed Möbius function mu(x) up to 10^5 we won't need more
+# table located right here ---> http://www.research.att.com/njas/sequences/A008683
+
+
+# just make getting the data file portable
+my $dir = Path::Class::File->new(__FILE__)->dir;
+my $mobdata = file($dir,'Möbius.data');
+
+#print $mobdata;
+
+@mu = split(/,/,"$mobdata");
 
 # cache by n, \Theta_n is the nth cyclotomic polynomial
 # the first cyclotomic polynomial is X-1
+
+
+
 my @cache = (Math::Polynomial->new(-1,+1)); 
 
 
 
 
-sub BUILD {
+
+
+
+
+
+sub new {
 	my ($self,$n) = @_;
+
+	$self->SUPER::new(@{ gen_pol($n)->[0] });
+}
+
+
+
+
+
+sub gen_pol {
+	my ($n) = @_;
 
 	###############################################################
 	# CASE I   n is a prime
 	# \Theta_n = 1 + X + X^2 + ... + X^(p-1)     for p a prime
 	return Math::Polynomial->new( (1) x $n ) 
 	if is_prime($n);
-
-
-	
 
 
 
@@ -52,7 +77,7 @@ sub BUILD {
 
 	my $first_pdiv = first { is_prime($_) } @factors; # first prime divisor of $n
 	# an unfortunate function name..
-	my $log = blog($n,$first_pdiv);
+	my $log = Math::BigInt::blog($n,$first_pdiv);
 
 	#this can also be done with (1,(p-1) zeros , 1 , (p-1) zeros, 1, <--- this should happen about m times)
 	#
@@ -70,16 +95,20 @@ sub BUILD {
 	#
 	# in the general case the cyclotomic polynomial can be computed using Mobius inversion
 
-	return
-	reduce { $a * $b }
-	(
-		map {
-				Math::Polynomial->new(1,(0) x ($_-1),1) ** 
-				$CM::Polynomial::Cyclotomic::mu[$n/$_]
-		} @factors
-	);
+	my $r = Math::Polynomial->new(1);
 
+	for my $d ( @factors ) {
+		next if $d == $n;
+
+		$r *= gen_pol($d);
+	};
+
+	return 
+
+	Math::Polynomial->new(-1,(0) x ($n - 1) , 1)
+	/
+	$r;
 }
 
 
-
+1;
