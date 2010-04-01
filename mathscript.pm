6284535e303e366e3b9cc4513b6ca23f4fbdf2e9
root <root@spx2.(none)>
2010-04-01 23:46:32 +0000

    additions

--- mathscript.pm
+++ mathscript.pm
@@ -25,6 +25,38 @@ has '_repl' => (
       # TODO: find a way for completion to work with Devel::REPL, tried the listed plugins, and they didn't work
       $r->eval('
 
+          sub help {
+          print "
+
+          mathshell is a shell for the modules in the CM::Permutation distribution
+
+          permutations:
+          ------------
+
+          cycle  - permutation cycle
+          perm   - permutation
+
+          polynomials:
+          -----------
+
+          pcyclo - nth cyclotomic polynomial
+          pcheby - nth chebyshev polynomial
+
+          groups:
+          ------
+
+          gsym   - S_n
+          galt   - A_n
+          gdih   - D_2n
+          gadd   - (Z_n,+)
+          gmul   - (Z_n,*)
+          gx     - group product
+
+          Ex:  gsym({n=>3})->compute()
+
+          ";
+          }
+
 
           sub perm {
           return CM::Permutation->new(@_);
@@ -34,14 +66,42 @@ has '_repl' => (
           return CM::Permutation::Cycle->new(@_);
           };
 
-          sub poly_cyclotomic {
+          sub pcyclo {
           return CM::Polynomial::Cyclotomic->new(@_);
           };
 
 
-          sub poly_chebyshev {
+          sub pcheby {
           return CM::Polynomial::Cyclotomic->new(@_);
           };
+
+          sub gsym {
+          return CM::Group::Sym->new(@_);
+          };
+
+          sub galt {
+          return CM::Group::Altern->new(@_);
+          };
+
+          sub gx {
+          return CM::Group::Product->new(@_);
+          };
+
+          sub gadd {
+          return CM::Group::ModuloAddition->new(@_);
+          };
+
+          sub gmul {
+          return CM::Group::ModuloMultiplication->new(@_);
+          };
+
+          sub gdih {
+          return CM::Group::Dihedral->new(@_);
+          };
+
+          sub q {
+          };
+
 
           ');
       $r;
