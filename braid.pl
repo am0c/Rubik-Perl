524141467ca2c0afa9dffca14539dbbb4b1628f7
root <root@spx2.(none)>
2010-04-04 00:36:24 +0000

    added permutation braid representation

--- braid.pl
+++ braid.pl
@@ -2,8 +2,42 @@ use GD::SVG;
 use List::AllUtils qw/max/;
 use feature ':5.10';
 use Carp;
+use strict;
+use warnings;
 
+# taken from tables_gfx branch
+my @map=(
+    sub{(         0,             0,   $_[0] * 255 )},
+    sub{(         0,     $_[0]*255,           255 )},
+    sub{(         0,           255, (1-$_[0])*255 )},
+    sub{( $_[0]*255,           255,             0 )},
+    sub{(       255, (1-$_[0])*255,             0 )},
+    sub{(       255,             0,     $_[0]*255 )},
+    sub{(       255,     $_[0]*255,           255 )},
+);
 
+sub ramp {
+    my( $img, $v, $vmin, $vmax ) = @_;
+
+    ## Peg $v to $vmax if it is greater than $vmax
+    $v = $vmax if $v > $vmax;
+    ## Or peg $v to $vmin if it is less tahn $vmin.
+    $v = $vmin if $v < $vmin;
+    ## Normalise $v relative to $vmax - $vmin
+    $v = 
+    ( $v    - $vmin ) /
+    ( $vmax - $vmin ) ;
+    ## Scale it to the range 0 .. 1784
+    $v *= 1785;
+
+    my @a = 
+    map { int($_) } 
+    $map[$v/255]->( ($v % 255) / 256 );
+
+    #print join(',',@a)."\n";
+
+    return $img->colorAllocate(@a);
+};
 
 
 sub draw {
@@ -11,8 +45,6 @@ sub draw {
     my $file = $_[1];
 
     my $circ_rad = 2;# circle radius for joints
-    my $img   = GD::SVG::Image->new(500,500);
-    $img->setThickness(4);
     my $d = 10;#distance between
     my $l = @perm; #height units of the image
     my $length = $d;# step length
@@ -34,87 +66,41 @@ my ($DOWN,$RIGHT,$LEFT)=
 # visual representation of a permutaion in the form of a braid
 # http://en.wikipedia.org/wiki/Braid_theory
 
-# taken from tables_gfx branch
-
-    my @map=(
-        sub{(         0,             0,   $_[0] * 255 )},
-        sub{(         0,     $_[0]*255,           255 )},
-        sub{(         0,           255, (1-$_[0])*255 )},
-        sub{( $_[0]*255,           255,             0 )},
-        sub{(       255, (1-$_[0])*255,             0 )},
-        sub{(       255,             0,     $_[0]*255 )},
-        sub{(       255,     $_[0]*255,           255 )},
-    );
-
-    sub ramp {
-        my( $v, $vmin, $vmax ) = @_;
-
-        ## Peg $v to $vmax if it is greater than $vmax
-        $v = $vmax if $v > $vmax;
-        ## Or peg $v to $vmin if it is less tahn $vmin.
-        $v = $vmin if $v < $vmin;
-        ## Normalise $v relative to $vmax - $vmin
-        $v = 
-        ( $v    - $vmin ) /
-        ( $vmax - $vmin ) ;
-        ## Scale it to the range 0 .. 1784
-        $v *= 1785;
-
-        my @a = 
-        map { int($_) } 
-        $map[$v/255]->( ($v % 255) / 256 );
-
-        #print join(',',@a)."\n";
-
-        return $img->colorAllocate(@a);
-    };
-############### cut here
 
 
-my @colours = map { ramp($_,1,~~@perm+2) } (1..@perm);
 #print join(' ',@colours);exit;
 
-# height of the image is determined by the transposition of the farthest apart elements( which is actually ~~@perm but ...)
-#my $l = max(map { abs($_-$perm($_)+1); } (1..@perm)); 
+
+        my $img   = GD::SVG::Image->new(500,500);
+        my @colours = map { ramp($img,$_,1,~~@perm+2) } (1..@perm);
+        $img->setThickness(4);
+        my ($lastx,$lasty) = (0,0); # last (x,y) position since the last step
+        # it's the colour currently used for painting;
+        my ($linestart,$lastdir,$colour);
+
+
+        my $step = sub  {
+            my ($dir) = @_;
+            #print "dir=$dir\n";
 
 
 
-
-#
-#  Graphical primitives:
-#  draw a segment of a line directly down/left/right one unit.
-#
-
-
-    my ($lastx,$lasty) = (0,0); # last (x,y) position since the last step
-    # it's the colour currently used for painting;
-    my ($linestart,$lastdir,$colour);
+            $img->ellipse($lastx,$lasty,$circ_rad,$circ_rad,$colour)
+            if(
+                $lastdir != -3 &&
+                !$linestart &&
+                $dir != $lastdir
+            );
 
 
-    sub step {
-        my ($dir) = @_;
-        #print "dir=$dir\n";
-
-        confess "x=$x" if $x<0;
-        confess "y=$x" if $y<0;
-
-
-        $img->ellipse($lastx,$lasty,$circ_rad,$circ_rad,$colour)
-        if(
-            $lastdir != -3 &&
-            !$linestart &&
-            $dir != $lastdir
-        );
-
-
-        given ($dir) {
-            when($DOWN) { $img->line($lastx,$lasty,$lastx    ,$lasty+$length,$colour);$lasty+=$length;            } # DOWN
-            when($LEFT) { $img->line($lastx,$lasty,$lastx-$d ,$lasty+$length,$colour);$lasty+=$length;$lastx-=$d; } # LEFT
-            when($RIGHT) { $img->line($lastx,$lasty,$lastx+$d ,$lasty+$length,$colour);$lasty+=$length;$lastx+=$d; } # RIGHT
+            given ($dir) {
+                when($DOWN) { $img->line($lastx,$lasty,$lastx    ,$lasty+$length,$colour);$lasty+=$length;            } # DOWN
+                when($LEFT) { $img->line($lastx,$lasty,$lastx-$d ,$lasty+$length,$colour);$lasty+=$length;$lastx-=$d; } # LEFT
+                when($RIGHT) { $img->line($lastx,$lasty,$lastx+$d ,$lasty+$length,$colour);$lasty+=$length;$lastx+=$d; } # RIGHT
+            };
+            $lastdir = $dir;
+            $linestart = 0;
         };
-        $lastdir = $dir;
-        $linestart = 0;
-    }
 
 
     for my $i (1..@perm-1) {
@@ -126,10 +112,10 @@ my @colours = map { ramp($_,1,~~@perm+2) } (1..@perm);
         $lastx = $i * $d; # this is the x position where the current strand starts
         $lasty = 0;
 
-        step(0);
-        step(-($i<=>$perm[$i])) for 1..(abs($i-$perm[$i]));
-        step(0) for 1..($l - abs($i-$perm[$i]));
-        step(0);
+        $step->(0);
+        $step->(-($i<=>$perm[$i])) for 1..(abs($i-$perm[$i]));
+        $step->(0) for 1..($l - abs($i-$perm[$i]));
+        $step->(0);
     };
 
     open my $fh,">$file";
@@ -139,6 +125,6 @@ my @colours = map { ramp($_,1,~~@perm+2) } (1..@perm);
 
 
 
-draw([0,6,1,2,4,5],'/tmp/stuff.svg');
+draw([0,2,1,6,4,5],'/tmp/stuff.svg');
 
 
--- lib/CM/Permutation.pm
+++ lib/CM/Permutation.pm
@@ -7,6 +7,10 @@ use Carp;
 use Data::Dumper;
 use Math::BigInt qw/blcm/;
 use Params::Validate;
+use GD::SVG;
+use List::AllUtils qw/max/;
+use feature ':5.10';
+
 
 # require is used here because there is the following dependency chain
 #
@@ -367,10 +371,123 @@ sub apply_sub {
     0..-1+@{$self->perm};
 }
 
+# taken from tables_gfx branch
+my @map=(
+    sub{(         0,             0,   $_[0] * 255 )},
+    sub{(         0,     $_[0]*255,           255 )},
+    sub{(         0,           255, (1-$_[0])*255 )},
+    sub{( $_[0]*255,           255,             0 )},
+    sub{(       255, (1-$_[0])*255,             0 )},
+    sub{(       255,             0,     $_[0]*255 )},
+    sub{(       255,     $_[0]*255,           255 )},
+);
+
+sub ramp {
+    my( $img, $v, $vmin, $vmax ) = @_;
+
+    ## Peg $v to $vmax if it is greater than $vmax
+    $v = $vmax if $v > $vmax;
+    ## Or peg $v to $vmin if it is less tahn $vmin.
+    $v = $vmin if $v < $vmin;
+    ## Normalise $v relative to $vmax - $vmin
+    $v = 
+    ( $v    - $vmin ) /
+    ( $vmax - $vmin ) ;
+    ## Scale it to the range 0 .. 1784
+    $v *= 1785;
+
+    my @a = 
+    map { int($_) } 
+    $map[$v/255]->( ($v % 255) / 256 );
+
+    #print join(',',@a)."\n";
+
+    return $img->colorAllocate(@a);
+};
+
 
 sub draw {
+    my ($self,$file) = @_;
+    my @perm = @{$self->perm};
 
-}
+    my $circ_rad = 2;# circle radius for joints
+    my $d = 10;#distance between
+    my $l = @perm; #height units of the image
+    my $length = $d;# step length
+
+
+
+=head1 DESCRIPTION
+
+Permutations can be viewed as braids but braids are much more general.
+(if you consider a permutation and apply it to itself it returns to the identical permutation, if you
+have a braid that's a transposition and apply it to itself you get a double-twist, one more time you get
+a triple twist and so forth this continues indefinitely).
+
+=cut
+
+my ($DOWN,$RIGHT,$LEFT)=
+(    0,    +1,   -1);
+
+# visual representation of a permutaion in the form of a braid
+# http://en.wikipedia.org/wiki/Braid_theory
+
+
+
+#print join(' ',@colours);exit;
+
+
+        my $img   = GD::SVG::Image->new(500,500);
+        my @colours = map { ramp($img,$_,1,~~@perm+2) } (1..@perm);
+        $img->setThickness(4);
+        my ($lastx,$lasty) = (0,0); # last (x,y) position since the last step
+        # it's the colour currently used for painting;
+        my ($linestart,$lastdir,$colour);
+
+
+        my $step = sub  {
+            my ($dir) = @_;
+            #print "dir=$dir\n";
+
+
+
+            $img->ellipse($lastx,$lasty,$circ_rad,$circ_rad,$colour)
+            if(
+                $lastdir != -3 &&
+                !$linestart &&
+                $dir != $lastdir
+            );
+
+
+            given ($dir) {
+                when($DOWN) { $img->line($lastx,$lasty,$lastx    ,$lasty+$length,$colour);$lasty+=$length;            } # DOWN
+                when($LEFT) { $img->line($lastx,$lasty,$lastx-$d ,$lasty+$length,$colour);$lasty+=$length;$lastx-=$d; } # LEFT
+                when($RIGHT) { $img->line($lastx,$lasty,$lastx+$d ,$lasty+$length,$colour);$lasty+=$length;$lastx+=$d; } # RIGHT
+            };
+            $lastdir = $dir;
+            $linestart = 0;
+        };
+
+
+    for my $i (1..@perm-1) {
+        #print "i=$i\n";
+        $colour = $colours[$i];
+        $linestart = 1;
+        $lastdir = -3;
+
+        $lastx = $i * $d; # this is the x position where the current strand starts
+        $lasty = 0;
+
+        $step->(0);
+        $step->(-($i<=>$perm[$i])) for 1..(abs($i-$perm[$i]));
+        $step->(0) for 1..($l - abs($i-$perm[$i]));
+        $step->(0);
+    };
+
+    open my $fh,">$file";
+    print $fh $img->svg;
+
+};
 
 
 
