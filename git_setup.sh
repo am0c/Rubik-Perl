ecd06e30db7b9374182319848a3f9e9e597c11b8
Stefan Petrea <stefan.petrea@gmail.com>
2010-04-18 17:24:12 +0300

    added script to checkout all the stuff in separate local branches

---
+++ git_setup.sh
@@ -0,0 +1,13 @@
+git checkout -t origin/dimino_fixes dimino_fixes
+#git branch -t origin/HEAD         
+git checkout -t origin/new_perm_algo_SJT            
+git checkout -t origin/tables_gfx 
+git checkout -t origin/dimino_fixes_and_morphisms   
+git checkout -t origin/isomorphism_invariants       
+git checkout -t origin/revert_order_elements        
+git checkout -t origin/distributed_solver           
+git checkout -t origin/solve_cube                   
+git checkout -t origin/galois_theory                
+git checkout -t origin/mathshell                    
+git checkout -t origin/special_polynomials          
+git checkout master
